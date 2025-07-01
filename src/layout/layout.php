3a05cc6674f4755f18f9b2429044bbd820b36c66
Venca <faltrein@gmail.com>
2025-07-01 12:00:03 +0200

    fixed: scss and css route

--- src/layout/layout.php
+++ src/layout/layout.php
@@ -5,7 +5,7 @@
     <title>Katalog knih</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
-    <link href="scss/main.scss" rel="stylesheet" />
+    <link href="public/css/main.css" rel="stylesheet" />
 </head>
 <body>
     <div class="page-wrapper">
